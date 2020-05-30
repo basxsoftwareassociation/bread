d58bf1d0635fd623d00b524a2b12b2f657704b94
sam <sam@basx.dev>
2020-05-30 10:43:45 +0700

    add: check for settings at startup

--- bread/apps.py
+++ bread/apps.py
@@ -1,4 +1,7 @@
+from collections.abc import Iterable
+
 from django.apps import AppConfig
+from django.conf import settings
 from django.core.checks import Error, register
 
 
@@ -20,6 +23,60 @@ class BreadConfig(AppConfig):
 def bread_config_check(app_configs, **kwargs):
     errors = []
     # TODO: check configured fields on views
-    if False:
-        errors.append(Error("an error", hint="A hint.", obj=None, id="bread.E001",))
+    required_settings = {
+        "CKEDITOR_UPLOAD_PATH": None,
+        "CKEDITOR_CONFIGS": None,
+        "STATICFILES_FINDERS": [
+            "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+            "compressor.finders.CompressorFinder",
+        ],
+        "AUTHENTICATION_BACKENDS": ["guardian.backends.ObjectPermissionBackend"],
+        "DATETIME_INPUT_FORMATS": ["%Y-%m-%dT%H:%M:%S", "%Y-%m-%dT%H:%M"],
+        "COMPRESS_PRECOMPILERS": None,
+        "CRISPY_TEMPLATE_PACK": "materialize",
+        "CRISPY_ALLOWED_TEMPLATE_PACKS": ["materialize"],
+        "LOGIN_URL": None,
+        "LOGIN_REDIRECT_URL": None,
+        "LOGOUT_REDIRECT_URL": None,
+    }
+    for setting, required_value in required_settings.items():
+        if hasattr(settings, setting):
+            if isinstance(required_value, Iterable):
+                current_value = getattr(settings, setting)
+                if not isinstance(current_value, Iterable):
+                    current_value = [current_value]
+                diff = set(required_value) - set(current_value)
+                if diff:
+                    errors.append(
+                        Error(
+                            f"setting {setting} is missing some entries",
+                            hint=f"{setting} must include {list(diff)}",
+                            obj=None,
+                            id=f"bread.setting_{setting}",
+                        )
+                    )
+            elif required_value is not None:
+                if getattr(settings, setting) != required_value:
+                    errors.append(
+                        Error(
+                            f"setting {setting} has wrong value",
+                            hint=f"Needs to be set to {required_value}",
+                            obj=None,
+                            id=f"bread.setting_{setting}",
+                        )
+                    )
+        else:
+            hint = ""
+            if isinstance(required_value, Iterable):
+                hint = f"Must contains the following values: {required_value}"
+            elif required_value is not None:
+                hint = f"Must be set to: {required_value}"
+            errors.append(
+                Error(
+                    f"setting {setting} is missing",
+                    hint=hint,
+                    obj=None,
+                    id=f"bread.setting_{setting}",
+                )
+            )
     return errors
