3080931e0eb074856920b03b0da0c5ea593e77e5
sam <sam@basx.dev>
2020-06-20 15:11:11 +0700

    fix: wrong ffmpeg args

--- bread/utils.py
+++ bread/utils.py
@@ -226,7 +226,7 @@ def get_audio_thumbnail(file_):
     outputname = os.path.join(settings.MEDIA_ROOT, thumbnail_name)
     outputurl = os.path.join(settings.MEDIA_URL, thumbnail_name)
     if not os.path.exists(outputname):
-        ffmpeg.input(inputname).trim(start=0, duration=60).output(
+        ffmpeg.input(inputname).filter("atrim", start=0, duration=60).output(
             outputname, format="mp3", audio_bitrate=64
         ).run()
     return outputurl
