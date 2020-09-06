c13334c21cf244d98b8d1b6a108afa7c75fc47e7
sam <sam@basx.dev>
2020-09-06 11:24:31 +0700

    add local middleware

--- bread/settings/required.py
+++ bread/settings/required.py
@@ -120,6 +120,7 @@ CELERY_CACHE_BACKEND = "django-cache"
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
