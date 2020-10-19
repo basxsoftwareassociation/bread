b48bfaeee329471eeb5f578af91b89b1b95448cc
sam <sam@basx.dev>
2020-10-19 13:19:27 +0700

    did: remove generated helper classes (were only transitional)

--- bread/layout/carbon_design/__init__.py
+++ bread/layout/carbon_design/__init__.py
@@ -34,36 +34,6 @@ from ..base import (
 ICONS = {"edit": "edit", "delete": "trash-can"}
 
 
-class Grid(DIV):
-    def __init__(self, *args, grid_mode=None, **kwargs):
-        """
-        grid_mode can be one of None, "narrow", "condensed"
-        """
-        kwargs["css_class"] = kwargs.get("css_class", "") + " bx--grid"
-        if grid_mode is not None:
-            kwargs["css_class"] += f" bx--grid-{grid_mode}"
-        super().__init__(*args, **kwargs)
-
-
-class Row(DIV):
-    def __init__(self, *args, **kwargs):
-        kwargs["css_class"] = kwargs.get("css_class", "") + " bx--row"
-        super().__init__(*args, **kwargs)
-
-
-class Col(DIV):
-    def __init__(self, *args, breakpoint=None, width=None, **kwargs):
-        """
-        breakpoint: Can be one of "sm", "md", "lg", "xlg", "max"
-        """
-        colclass = "bx--col"
-        if breakpoint is not None:
-            assert width is not None, "When breakpoint is given, width is also required"
-            colclass += f"-{breakpoint}-{width}"
-        kwargs["css_class"] = kwargs.get("css_class", "") + f" {colclass}"
-        super().__init__(*args, **kwargs)
-
-
 class InlineLayout(Layout):
     """Used to render inline forms"""
 
--- bread/layout/carbon_design/components/grid.py
+++ bread/layout/carbon_design/components/grid.py
@@ -1,13 +1,31 @@
-from crispy_forms.utils import TEMPLATE_PACK
-
-from .. import Component
+from ..base import DIV, INPUT
 
 
-class Grid(Component):
-    template = "carbon_design/components/grid.html"
+class Grid(DIV):
+    def __init__(self, *args, grid_mode=None, **kwargs):
+        """
+        grid_mode can be one of None, "narrow", "condensed"
+        """
+        kwargs["css_class"] = kwargs.get("css_class", "") + " bx--grid"
+        if grid_mode is not None:
+            kwargs["css_class"] += f" bx--grid-{grid_mode}"
+        super().__init__(*args, **kwargs)
 
-    def __init__(self, *fields, **kwargs):
-        super().__init__(*fields, **kwargs)
 
-    def render(self, form, form_style, context, template_pack=TEMPLATE_PACK, **kwargs):
-        return super().render(form, form_style, context, template_pack, **kwargs)
+class Row(DIV):
+    def __init__(self, *args, **kwargs):
+        kwargs["css_class"] = kwargs.get("css_class", "") + " bx--row"
+        super().__init__(*args, **kwargs)
+
+
+class Col(DIV):
+    def __init__(self, *args, breakpoint=None, width=None, **kwargs):
+        """
+        breakpoint: Can be one of "sm", "md", "lg", "xlg", "max"
+        """
+        colclass = "bx--col"
+        if breakpoint is not None:
+            assert width is not None, "When breakpoint is given, width is also required"
+            colclass += f"-{breakpoint}-{width}"
+        kwargs["css_class"] = kwargs.get("css_class", "") + f" {colclass}"
+        super().__init__(*args, **kwargs)
