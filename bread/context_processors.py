271c14e7cefe670f3f6270e94926192e6f60b491
sam <sam@basx.dev>
2020-11-04 09:45:54 +0700

    did: cleanup from removing materialize

--- bread/context_processors.py
+++ bread/context_processors.py
@@ -1,15 +1,8 @@
-from crispy_forms.utils import TEMPLATE_PACK
 from django.conf import settings
 
 
 def bread_context(request):
-    ret = {
-        "TEMPLATE_PACK": TEMPLATE_PACK,
-        "TEMPLATE_PACK_BASE": f"{TEMPLATE_PACK}/base.html",
-    }
-    if TEMPLATE_PACK == "carbon_design":
-        ret.update(settings.CARBON_DESIGN_CONTEXT)
-
+    ret = {}
     return ret
 
 
