91deeaafbbfbb6759989a1c3773901612cfbe31a
sam <sam@basx.dev>
2020-11-14 17:42:23 +0700

    del: unnecessary rect elements behind icons

--- bread/static/design/carbon_design/icons/convert_raw.sh
+++ bread/static/design/carbon_design/icons/convert_raw.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir -p flat/$(dirname $1)
-xpath -e '//svg/*[not(self::defs) and not(self::style)]' $1 > flat/$1
+xpath -e '//svg/*[not(self::defs) and not(self::style) and not(self::rect and @class="st0")]' $1 > flat/$1
