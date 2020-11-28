1e5560f052ae465c1909bddd046f3d003b81b9b8
Sam <sam@basx.dev>
2020-11-28 17:10:02 +0700

    did: move html_id to htmlgenerator package

--- bread/layout/base.py
+++ bread/layout/base.py
@@ -2,20 +2,6 @@ import htmlgenerator
 from bread.utils import pretty_fieldname, pretty_modelname
 
 
-def html_id(object):
-    """Generate a unique HTML id from an object"""
-    # not sure how bad this one is regarding possible security issues,
-    # it might leak memory layout to the frontend, at least in the CPython implementation
-    # but I think it is sufficient safe
-    # reasoning:
-    # - id: We want a guaranteed unique ID. Since the lifespan of an HTML-response is shorted than that of the working process, this should be fine. Python might re-use objects and their according memory, but it seems unlikely that this will be an issue.
-    # - str: passing an int (from id) into the hash-function will result in the int beeing passed back. We need to convert the id to a string in order have hash doing some real hashing
-    # - hash: Prevent the leaking of any memory layout information. hash is of course not secure but should not be trivial to reverse.
-    # - str: Because html-ids are strings we convert again to string
-    # - [1:]: in case there is a leading "-" we remove the first character
-    return str(hash(str(id(object))))[1:]
-
-
 class ModelContext(htmlgenerator.ValueProvider):
     """Provides a model to marked child elements"""
 
--- bread/layout/components/search.py
+++ bread/layout/components/search.py
@@ -1,27 +1,26 @@
-import htmlgenerator
+import htmlgenerator as hg
 from django.utils.translation import gettext_lazy as _
 
-from ..base import html_id
 from .icon import Icon
 
 
-class Search(htmlgenerator.DIV):
+class Search(hg.DIV):
     def __init__(self, size="xl", **kwargs):
         kwargs["_class"] = kwargs.get("_class", "") + f" bx--search bx--search--{size}"
         kwargs["data-search"] = True
         kwargs["role"] = "search"
 
-        inputid = "search__" + html_id(self)
+        inputid = "search__" + hg.html_id(self)
         super().__init__(
-            htmlgenerator.LABEL(_("Search"), _class="bx--label", _for=inputid),
-            htmlgenerator.INPUT(
+            hg.LABEL(_("Search"), _class="bx--label", _for=inputid),
+            hg.INPUT(
                 _class="bx--search-input",
                 type="text",
                 id=inputid,
                 placeholder=_("Search"),
             ),
             Icon("search", size=16, _class="bx--search-magnifier", aria_hidden="true"),
-            htmlgenerator.BUTTON(
+            hg.BUTTON(
                 Icon("close", size=20, _class="bx--search-clear"),
                 _class="bx--search-close bx--search-close--hidden",
                 title=_("Clear search input"),
