95e895dc1b778922a037f43e56a76d3075a6fac5
Sam <sam@basx.dev>
2020-11-30 15:15:49 +0700

    fix: clear-button does also submit forms

--- bread/layout/components/search.py
+++ bread/layout/components/search.py
@@ -5,26 +5,29 @@ from .icon import Icon
 
 
 class Search(hg.DIV):
-    def __init__(self, size="xl", **kwargs):
+    def __init__(self, size="xl", widgetattributes=None, **kwargs):
         kwargs["_class"] = kwargs.get("_class", "") + f" bx--search bx--search--{size}"
         kwargs["data-search"] = True
         kwargs["role"] = "search"
 
-        inputid = "search__" + hg.html_id(self)
+        attributes = {
+            "id": "search__" + hg.html_id(self),
+            "_class": "bx--search-input",
+            "type": "text",
+            "placeholder": _("Search"),
+            **(widgetattributes or {}),
+        }
+
         super().__init__(
-            hg.LABEL(_("Search"), _class="bx--label", _for=inputid),
-            hg.INPUT(
-                _class="bx--search-input",
-                type="text",
-                id=inputid,
-                placeholder=_("Search"),
-            ),
+            hg.LABEL(_("Search"), _class="bx--label", _for=attributes["id"]),
+            hg.INPUT(**attributes),
             Icon("search", size=16, _class="bx--search-magnifier", aria_hidden="true"),
             hg.BUTTON(
                 Icon("close", size=20, _class="bx--search-clear"),
                 _class="bx--search-close bx--search-close--hidden",
                 title=_("Clear search input"),
                 aria_label=_("Clear search input"),
+                type="button",
             ),
             **kwargs,
         )
