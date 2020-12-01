f8119856ca6a5b9a4fdcff55da5c92df30d64409
Sam <sam@basx.dev>
2020-12-01 11:41:46 +0700

    fix: by default all views should only be accessible for logged in users

--- bread/middleware.py
+++ bread/middleware.py
@@ -1,17 +1,12 @@
 from django.http import HttpResponseRedirect
-from django.urls import resolve, reverse
+from django.urls import reverse
 
 
 def RequireAuthenticationMiddleware(get_response):
     def middleware(request):
         """All urls in the bread namespace require authentication"""
 
-        match = resolve(request.path)
-        if (
-            match.namespaces
-            and match.namespaces[0] == "bread"
-            and not request.user.is_authenticated
-        ):
+        if not request.user.is_authenticated:
             return HttpResponseRedirect(reverse("login") + "?next=" + request.path)
         return get_response(request)
 
