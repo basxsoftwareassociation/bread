0545e7a5e6658005c48ae439553f29db2e10bf2c
Sam <sam@basx.dev>
2020-12-07 21:43:22 +0700

    fix: flake8

--- bread/tests/datetimeformatstring.py
+++ bread/tests/datetimeformatstring.py
@@ -1,11 +1,11 @@
 import datetime
 import random
 
-from bread.utils.datetimeformatstring import to_php_formatstr
-
 from django.conf import settings
-from django.test import Client, TestCase
+from django.test import TestCase
 from django.utils.dateformat import format
+
+from bread.utils.datetimeformatstring import to_php_formatstr
 
 
 class ToPHPFormatstrTest(TestCase):
