4418133d5cfe9f3fe881af289558ea219c2bee08
Sam <sam@basx.dev>
2021-05-25 12:30:02 +0700

    fix: make comment clearer

--- bread/layout/components/form.py
+++ bread/layout/components/form.py
@@ -195,7 +195,7 @@ class FormsetField(FormChild, hg.BaseElement):
         **formsetfactory_kwargs,
     ):
         """
-        :param str fieldname: The fieldname which should be used for an formset, in general a n-to-many field
+        :param str fieldname: The fieldname which should be used for an formset, in general a one-to-many or many-to-many field
         :param list inlinefieldnames: A list of strings or objects. Strings are converted to DataTableColumn, objects are passed on as they are
         :param str formname: Name of the surounding django-form object in the context
         :param str title: Datatable title, automatically generated from form if None
