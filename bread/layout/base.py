9bd16b80958fc1cba9e1badaf7bdcc21ca848d51
Sam <sam@basx.dev>
2021-06-03 10:43:26 +0700

    did: improve model attribute label generation

--- bread/layout/base.py
+++ bread/layout/base.py
@@ -1,14 +1,26 @@
 import htmlgenerator as hg
 
-from bread.utils import pretty_modelname, resolve_modellookup
+from bread.utils import pretty_modelname
 from bread.utils.urls import reverse_model
 
 from ..formatters import format_value
 
 
 def fieldlabel(model, accessor):
-    label = resolve_modellookup(model, accessor)[-1]
-    return getattr(label, "verbose_name", None) or label
+    field = hg.resolve_lookup(model, accessor)
+    try:
+        return field.verbose_name  # manualy set
+    except AttributeError:
+        try:
+            return field.field.verbose_name  # model field
+        except AttributeError:
+            try:
+                return field.__name__  # method
+            except AttributeError:
+                try:
+                    return field.fget.__name__  # property
+                except AttributeError:
+                    return field
 
 
 def objectaction(object, action, *args, **kwargs):
