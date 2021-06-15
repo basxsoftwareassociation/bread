501dcfd8d05552f47e65ad3300207cd55d29efbb
Pascal Wiesmann <pascal.wiesmann@outlook.com>
2021-06-15 15:45:33 +0200

    move tag out of bx-search component

--- bread/layout/components/search_select.py
+++ bread/layout/components/search_select.py
@@ -8,12 +8,13 @@ from .loading import Loading
 from .tag import Tag
 
 
-class SearchSelect(hg.BaseElement):
+class SearchSelect(hg.DIV):
     def __init__(
         self,
         search_view,
+        boundfield,
         query_urlparameter="q",
-        size="xl",
+        size="lg",
         searchinput_widgetattributes=None,
         widgetattributes=None,
         **elementattributes,
@@ -22,13 +23,12 @@ class SearchSelect(hg.BaseElement):
         # This works inside a formset. Might need to be changed for other usages.
         current_selection = (
             getattr(
-                elementattributes["boundfield"].form.instance,
+                boundfield.form.instance,
                 elementattributes["fieldname"],
             )
             if current_selection_id
             else None
         )
-        del elementattributes["boundfield"]
         elementattributes["_class"] = (
             elementattributes.get("_class", "") + f" bx--search bx--search--{size}"
         )
@@ -39,7 +39,6 @@ class SearchSelect(hg.BaseElement):
             "id": "search__" + hg.html_id(self),
             "_class": "bx--search-input",
             "type": "text",
-            "style": "width: auto;",
             **(searchinput_widgetattributes or {}),
         }
 
@@ -52,49 +51,60 @@ class SearchSelect(hg.BaseElement):
             },
         )
         super().__init__(
+            Tag(
+                current_selection,
+                id=widgetattributes["id"] + "-tag",
+                style=hg.If(
+                    current_selection_id == "",
+                    hg.BaseElement("visibility: hidden"),
+                ),
+                onclick="return false;",
+            ),
             hg.DIV(
-                hg.LABEL(
-                    _("Search"), _class="bx--label", _for=search_widget_attributes["id"]
-                ),
-                hg.INPUT(
-                    hx_get=url,
-                    hx_trigger="changed, keyup changed delay:500ms",
-                    hx_target=f"#{resultcontainerid}",
-                    hx_indicator=f"#{resultcontainerid}-indicator",
-                    name=query_urlparameter,
-                    **search_widget_attributes,
-                ),
-                Tag(
-                    current_selection,
-                    id=widgetattributes["id"] + "-tag",
-                    style=hg.If(
-                        current_selection_id == "",
-                        hg.BaseElement("visibility: hidden"),
+                hg.DIV(
+                    hg.LABEL(
+                        _("Search"),
+                        _class="bx--label",
+                        _for=search_widget_attributes["id"],
                     ),
-                    onclick="return false;",
+                    hg.INPUT(
+                        hx_get=url,
+                        hx_trigger="changed, keyup changed delay:500ms",
+                        hx_target=f"#{resultcontainerid}",
+                        hx_indicator=f"#{resultcontainerid}-indicator",
+                        name=query_urlparameter,
+                        **search_widget_attributes,
+                    ),
+                    Icon(
+                        "search",
+                        size=16,
+                        _class="bx--search-magnifier",
+                        aria_hidden="true",
+                    ),
+                    hg.BUTTON(
+                        Icon("close", size=20, _class="bx--search-clear"),
+                        _class="bx--search-close bx--search-close--hidden",
+                        title=_("Clear search input"),
+                        aria_label=_("Clear search input"),
+                        type="button",
+                        onclick=f"document.getElementById('{resultcontainerid}').innerHTML = '';",
+                    ),
+                    hg.INPUT(_type="hidden", **widgetattributes),
+                    hg.DIV(
+                        Loading(small=True),
+                        id=f"{resultcontainerid}-indicator",
+                        _class="htmx-indicator",
+                        style="position: absolute; right: 2rem",
+                    ),
+                    **elementattributes,
                 ),
-                hg.BUTTON(
-                    Icon("close", size=20, _class="bx--search-clear"),
-                    _class="bx--search-close bx--search-close--hidden",
-                    title=_("Clear search input"),
-                    aria_label=_("Clear search input"),
-                    type="button",
-                    onclick=f"document.getElementById('{resultcontainerid}').innerHTML = '';",
-                ),
-                hg.INPUT(_type="hidden", **widgetattributes),
                 hg.DIV(
-                    Loading(small=True),
-                    id=f"{resultcontainerid}-indicator",
-                    _class="htmx-indicator",
-                    style="position: absolute; right: 2rem",
+                    hg.DIV(
+                        id=resultcontainerid,
+                        _style="width: 100%; position: absolute; z-index: 999",
+                    ),
+                    style="width: 100%; position: relative",
                 ),
-                **elementattributes,
             ),
-            hg.DIV(
-                hg.DIV(
-                    id=resultcontainerid,
-                    _style="width: 100%; position: absolute; z-index: 999",
-                ),
-                style="width: 100%; position: relative",
-            ),
+            style="display: flex;",
         )
