3b16ce069c619dc572f1142343818aedf2b027b5
Sam <sam@basx.dev>
2021-08-02 17:46:35 +0700

    fix: not working with celery>=5

--- bread/contrib/workflows/apps.py
+++ bread/contrib/workflows/apps.py
@@ -1,4 +1,4 @@
-from celery.decorators import periodic_task
+from celery import Task, shared_task
 from django.apps import AppConfig
 
 from . import settings
@@ -8,7 +8,22 @@ class WorkflowsConfig(AppConfig):
     name = "bread.contrib.workflows"
 
     def ready(self):
-        periodic_task(run_every=settings.WORKFLOW_BEAT)(update_workflows)
+        shared_task(base=WorkflowUpdate, run_every=settings.WORKFLOW_BEAT)(
+            update_workflows
+        )
+
+
+class WorkflowUpdate(Task):
+    @classmethod
+    def on_bound(cls, app):
+        app.conf.beat_schedule[cls.name] = {
+            "task": cls.name,
+            "schedule": cls.run_every.total_seconds(),
+            "args": (),
+            "kwargs": {},
+            "options": getattr(cls, "options", {}),
+            "relative": getattr(cls, "relative", False),
+        }
 
 
 def update_workflows():
