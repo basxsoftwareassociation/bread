faef53fd4edefb6d69b9afbe7167072701ef0451
Sam <sam@basx.dev>
2021-11-25 22:10:28 +0700

    fix: isort

--- bread/layout/base.py
+++ bread/layout/base.py
@@ -1,8 +1,6 @@
 import datetime
 
 import htmlgenerator as hg
-from bread.utils import pretty_modelname, resolve_modellookup
-from bread.utils.urls import reverse_model
 from django.conf import settings
 from django.db import models
 from django.db.models import ManyToOneRel
@@ -11,6 +9,9 @@ from django.template.context import _builtin_context_processors
 from django.utils.formats import localize
 from django.utils.module_loading import import_string
 from django.utils.timezone import localtime
+
+from bread.utils import pretty_modelname, resolve_modellookup
+from bread.utils.urls import reverse_model
 
 from ..formatters import format_value
 
