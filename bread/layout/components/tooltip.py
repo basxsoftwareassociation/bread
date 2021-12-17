cc26828dd3a770e52af182e0e8231d61da87a539
Saksinkarn Petchkuljinda <dward.the.2nd@gmail.com>
2021-12-18 00:46:39 +0700

    added class definitions

---
+++ bread/layout/components/tooltip.py
@@ -0,0 +1,13 @@
+import htmlgenerator as hg
+
+
+class DefinitionTooltip(hg.HTMLElement):
+    tag = "bx-tooltip-definition"
+
+
+class IconTooltip(hg.HTMLElement):
+    tag = "bx-tooltip-icon"
+
+
+class InteractionTooltip(hg.HTMLElement):
+    tag = "bx-tooltip"
