c4ea53846122f0fd8984e1e8aeb6a01559c05586
Saksinkarn Petchkuljinda <dward.the.2nd@gmail.com>
2022-01-10 12:07:33 +0700

    simplify the code

--- bread/layout/error.py
+++ bread/layout/error.py
@@ -12,32 +12,21 @@ def error_layout(
     status_code: int,
     status_title: str,
     description: Union[str, hg.BaseElement],
-    exception: BaseException = None,
     exception_detail: str = None,
     redirect_link: Link = None,
 ):
-    ret = hg.BaseElement(
+    return hg.BaseElement(
         hg.H1(f"{status_code}: {status_title}", style="margin-bottom: 1rem;"),
         hg.P(
             description,
             style="margin-bottom: 1rem;",
         ),
-    )
-
-    if exception or exception_detail:
-        ex_msg = ""
-        if exception_detail or hasattr(exception, "message"):
-            if exception_detail is not None:
-                ex_msg = exception_detail
-            elif hasattr(exception, "message"):
-                ex_msg = exception.message
-
-            ret.append(
+        hg.If(
+            exception_detail,
+            hg.BaseElement(
                 hg.H4("Detail", style="margin-bottom: 1rem;"),
-            )
-            ret.append(
                 hg.DIV(
-                    ex_msg,
+                    exception_detail,
                     style=(
                         "border: 1px solid grey;"
                         "padding: 1rem;"
@@ -45,15 +34,7 @@ def error_layout(
                         "margin-bottom: 1rem;"
                     ),
                 ),
-            )
-
-    if redirect_link is not None:
-        ret.append(
-            Button.fromlink(redirect_link),
-        )
-    else:
-        ret.append(
-            Button.fromlink(Link(label="Back to homepage", href="/")),
-        )
-
-    return ret
+            ),
+        ),
+        Button.fromlink(Link(label="Back to homepage", href="/")),
+    )
--- bread/views/error.py
+++ bread/views/error.py
@@ -1,78 +1,77 @@
 import sys
 
 import htmlgenerator as hg
-from django.http import HttpResponse, HttpResponseNotFound
 from django.utils.translation import gettext_lazy as _
 
 from bread.layout import error as layouts
 
 
 def view400(request, exception):
-    return HttpResponse(
-        layouts.error_layout(
-            request,
-            status_code=400,
-            status_title=_("Bad request"),
-            description=_(
-                "The server cannot process the request due to something that is "
-                "perceived to be a client error."
-            ),
-            exception=exception,
+    response = layouts.error_layout(
+        request,
+        status_code=400,
+        status_title=_("Bad request"),
+        description=_(
+            "The server cannot process the request due to something that is "
+            "perceived to be a client error."
         ),
-        status=400,
+        exception_detail=exception.message if hasattr(exception, "message") else None,
     )
+    response.status_code = 400
+
+    return response
 
 
 # As I understand, we might not need this page because the website will eventually
 # redirect users to the default page. But I'll still make it in case it's necessary.
 def view403(request, exception):
-    return HttpResponse(
-        layouts.error_layout(
-            request,
-            status_code=403,
-            status_title=_("Forbidden"),
-            description=_("You don't have permission to access this resource."),
-            exception=exception,
-        ),
-        status=403,
+    response = layouts.error_layout(
+        request,
+        status_code=403,
+        status_title=_("Forbidden"),
+        description=_("You don't have permission to access this resource."),
+        exception_detail=exception.message if hasattr(exception, "message") else None,
     )
+    response.status_code = 403
+
+    return response
 
 
 def view404(request, exception):
-    return HttpResponseNotFound(
-        layouts.error_layout(
-            request,
-            status_code=404,
-            status_title=_("Page not found"),
-            description=hg.BaseElement(
-                hg.mark_safe(
-                    _(
-                        "The path %s does not appear to be any existing page of our website."
-                    )
-                    % f"<strong>{request.path}</strong>"
-                )
-            ),
-            exception=exception,
-        )
+    response = layouts.error_layout(
+        request,
+        status_code=404,
+        status_title=_("Page not found"),
+        description=hg.BaseElement(
+            hg.mark_safe(
+                _("The path %s does not appear to be any existing page of our website.")
+                % f"<strong>{request.path}</strong>"
+            )
+        ),
+        exception_detail=exception.message if hasattr(exception, "message") else None,
     )
+    response.status_code = 404
+
+    return response
 
 
 def view500(request):
     exec_info = sys.exc_info()
-    return HttpResponse(
-        layouts.error_layout(
-            request,
-            status_code=500,
-            status_title=_("Internal Server Error"),
-            description=hg.BaseElement(
-                hg.mark_safe(
-                    _(
-                        "There's a problem occurred within the server related to path %s."
-                        "<br>If the problem persists, please contact admin."
-                    )
-                    % f"<strong>{request.path}</strong>"
+    response = layouts.error_layout(
+        request,
+        status_code=500,
+        status_title=_("Internal Server Error"),
+        description=hg.BaseElement(
+            hg.mark_safe(
+                _(
+                    "There's a problem occurred within the server related to path %s."
+                    "<br>If the problem persists, please contact admin."
                 )
-            ),
-            exception_detail=f"{str(exec_info[0].__name__)}: {exec_info[1]}",
-        )
+                % f"<strong>{request.path}</strong>"
+            )
+        ),
+        exception_detail=f"{str(exec_info[0].__name__)}: {exec_info[1]}",
     )
+    response.status_code = 500
+
+    return response
