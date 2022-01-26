48a1df46d209f766f737e9ce3702c06ead2949b9
Pascal Wiesmann <pascal.wiesmann@outlook.com>
2022-01-26 16:42:03 +0200

    remove DocumentTemplate.filter

--- bread/contrib/document_templates/models.py
+++ bread/contrib/document_templates/models.py
@@ -2,8 +2,6 @@ from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-from bread.contrib.reports.fields.queryfield import QuerysetField
-
 
 class DocumentTemplate(models.Model):
     name = models.CharField(_("Name"), max_length=255)
@@ -11,7 +9,6 @@ class DocumentTemplate(models.Model):
         ContentType,
         on_delete=models.PROTECT,
     )
-    filter = QuerysetField(_("Filter"), modelfieldname="model")
     file = models.FileField(upload_to="document_templates/")
 
     def __str__(self):
