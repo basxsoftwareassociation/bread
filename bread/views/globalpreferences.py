80ed22d245d25a4f8c264097181438e0207fa6f0
Pascal Wiesmann <pascal.wiesmann@outlook.com>
2022-01-26 16:27:05 +0200

    simplify PreferencesView by inheriting from BreadView

--- bread/views/globalpreferences.py
+++ bread/views/globalpreferences.py
@@ -1,40 +1,19 @@
-import typing
-
 import htmlgenerator as hg
-from django.conf import settings
 from django.contrib.messages.views import SuccessMessageMixin
-from django.utils.module_loading import import_string
 from dynamic_preferences import views as preferences_views
 from dynamic_preferences.forms import GlobalPreferenceForm
 from dynamic_preferences.registries import global_preferences_registry
 
 from bread import layout as breadlayout
-from bread import menu
+from bread.views import BreadView
 
 
-class PreferencesView(SuccessMessageMixin, preferences_views.PreferenceFormView):
-
-    layout: typing.Optional[hg.BaseElement] = None
-    _layout_cached: typing.Optional[hg.BaseElement] = None
-    page_layout: typing.Optional[
-        typing.Callable[[menu.Menu, hg.BaseElement], hg.BaseElement]
-    ] = None
+class PreferencesView(
+    SuccessMessageMixin, BreadView, preferences_views.PreferenceFormView
+):
     success_message = "Preferences updated"
     form_class = GlobalPreferenceForm
     registry = global_preferences_registry
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.page_layout = (
-            kwargs.get("page_layout", getattr(self, "page_layout"))
-            or settings.DEFAULT_PAGE_LAYOUT
-        )
-        if isinstance(self.page_layout, str):
-            self.page_layout = import_string(self.page_layout)
-
-    def get_page_layout(self, menu, content_layout):
-        return self.page_layout(menu, content_layout)
 
     def get_layout(self):
         section_fields = {}
@@ -61,10 +40,3 @@ class PreferencesView(SuccessMessageMixin, preferences_views.PreferenceFormView)
             ],
             breadlayout.forms.helpers.Submit(),
         )
-
-    def render_to_response(self, context, **response_kwargs):
-        response_kwargs.setdefault("content_type", self.content_type)
-        ret = self.get_layout()
-        ret = self.get_page_layout(menu.main, ret)
-
-        return breadlayout.render(self.request, ret, context, **response_kwargs)
