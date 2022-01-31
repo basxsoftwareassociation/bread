8ce541711b268cb827dbed8cfef89a814353c088
Sam <sam@basx.dev>
2022-01-31 18:22:40 +0700

    add: better feedback to action runs by splitting all actions into single celery tasks

--- bread/contrib/triggers/apps.py
+++ bread/contrib/triggers/apps.py
@@ -1,5 +1,6 @@
 import datetime
 
+from celery import shared_task
 from django.apps import AppConfig
 from django.conf import settings
 from django.utils import timezone
@@ -15,7 +16,6 @@ class TriggersConfig(AppConfig):
 
     def ready(self):
 
-        from celery import shared_task
         from django.db.models.signals import post_delete, post_save
 
         from bread.utils.celery import RepeatedTask
@@ -42,8 +42,23 @@ def datachange_trigger(model, instance, type):
     for trigger in DataChangeTrigger.objects.filter(
         model=ContentType.objects.get_for_model(model), type=type, enable=True
     ):
-        if trigger.filter.queryset.filter(id=instance.id).exists():
-            get_concrete_instance(trigger.action).run(instance)
+        if trigger.filter.queryset.filter(pk=instance.pk).exists():
+            # delay execution a bit as the trigger may run immediately even though
+            # the current request has not finished (and therefore not commited to DB yet)
+            run_action.apply_async(
+                (trigger.action.pk, instance._meta.label, instance.pk), countdown=5
+            )
+
+
+@shared_task
+def run_action(action_pk, modelname, instance_pk):
+    from django.apps import apps
+
+    from .models import Action
+
+    get_concrete_instance(Action.objects.get(pk=action_pk)).run(
+        apps.get_model(modelname).objects.get(pk=instance_pk)
+    )
 
 
 def periodic_trigger():
@@ -57,4 +72,6 @@ def periodic_trigger():
                 td is not None
                 and timezone.now() <= td < timezone.now() + TRIGGER_PERIOD
             ):
-                get_concrete_instance(trigger.action).run(instance)
+                run_action.apply_async(
+                    (trigger.action.pk, instance._meta.label, instance.pk)
+                )
