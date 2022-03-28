17a0d046f5162517bc73a2b06c919d9ab48af8d6
Sam <sam@basx.dev>
2022-03-28 15:44:12 +0700

    did: improve document template interfacing

--- bread/contrib/document_templates/models.py
+++ bread/contrib/document_templates/models.py
@@ -1,8 +1,11 @@
+import io
 from typing import Union
 
 import htmlgenerator as hg
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from docxtpl import DocxTemplate
 
 from bread.utils import ModelHref
 
@@ -11,15 +14,47 @@ class DocumentTemplate(models.Model):
     name = models.CharField(_("Name"), max_length=255)
     file = models.FileField(upload_to="document_templates/")
 
+    model = models.ForeignKey(
+        ContentType, on_delete=models.PROTECT, verbose_name=_("Model")
+    )
+
+    def render_with(self, object):
+        docxtpl_template = DocxTemplate(self.file.path)
+        docxtpl_template.render(
+            {
+                variable.name: hg.resolve_lookup(object, variable.value)
+                for variable in self.variables.all()
+            }
+        )
+
+        buf = io.BytesIO()
+        docxtpl_template.save(buf)
+        buf.seek(0)
+        return buf
+
+    def generate_document_url(self, obj: Union[hg.Lazy, models.Model]):
+        return ModelHref.from_object(
+            self,
+            "generate_document",
+            kwargs={"object_pk": obj.pk},
+        )
+
     def __str__(self):
         return self.name
 
-    def generate_document_url(self, obj: Union[hg.Lazy, models.Model]):
-        return ModelHref(
-            self,
-            "generate-document",
-            kwargs={
-                "model_string": obj._meta.label_lower,
-                "object_id": obj.pk,
-            },
-        )
+    class Meta:
+        verbose_name = _("Document Template")
+        verbose_name_plural = _("Document Templates")
+
+
+class DocumentTemplateVariable(models.Model):
+    document_template = models.ForeignKey(
+        DocumentTemplate, on_delete=models.CASCADE, related_name="variables"
+    )
+    name = models.CharField(_("Name"), max_length=255)
+    value = models.CharField(_("Value"), max_length=255)
+    raw_value = models.BooleanField(_("Raw value"), default=False)
+
+    class Meta:
+        verbose_name = _("Variable")
+        verbose_name_plural = _("Variables")
