058d68aba60bd099776baffd42e23bd29e565eb0
Sam <sam@basx.dev>
2022-04-07 10:56:19 +0700

    fix: allow icon module to be imported early

--- bread/layout/components/icon.py
+++ bread/layout/components/icon.py
@@ -1,21 +1,10 @@
-import logging
 import os
 
 import htmlgenerator as hg
 from django.contrib.staticfiles import finders
 from django.utils.html import mark_safe
 
-logger = logging.getLogger(__name__)
-
 RAW_ICON_BASE_PATH = "design/carbon_design/icons/flat/raw_32/"
-ABSOLUTE_PATH = finders.find(RAW_ICON_BASE_PATH)
-ICONS = {}
-
-for name in os.listdir(ABSOLUTE_PATH):
-    iconpath = os.path.join(ABSOLUTE_PATH, name)
-    if os.path.isfile(iconpath):
-        with open(iconpath) as f:
-            ICONS[name.replace(".svg", "")] = mark_safe(f.read())
 
 
 class Icon(hg.SVG):
@@ -25,12 +14,16 @@ class Icon(hg.SVG):
     In order to see the name which should be passed to this template tag, click on "Download SVG"
     for an icon and use the filename without the attribte, e.g. "thunderstorm--severe"."""
 
+    ICONS = None
+
     def __init__(
         self,
         name,
         size=None,
         **attributes,
     ):
+        if Icon.ICONS is None:
+            Icon.ICONS = loadicons(RAW_ICON_BASE_PATH)
         attributes["viewBox"] = "0 0 32 32"
         attributes["preserveAspectRatio"] = "xMidYMid meet"
         attributes["focusable"] = "false"
@@ -42,4 +35,18 @@ class Icon(hg.SVG):
             attributes["width"] = size
             attributes["height"] = size
         self.name = name
-        super().__init__(hg.F(lambda c: ICONS[hg.resolve_lazy(name, c)]), **attributes)
+        super().__init__(
+            hg.F(lambda c: Icon.ICONS[hg.resolve_lazy(name, c)]), **attributes
+        )
+
+
+def loadicons(basepath):
+    absolute_path = finders.find(basepath)
+    icons = {}
+
+    for name in os.listdir(absolute_path):
+        iconpath = os.path.join(absolute_path, name)
+        if os.path.isfile(iconpath):
+            with open(iconpath) as f:
+                icons[name.replace(".svg", "")] = mark_safe(f.read())
+    return icons
