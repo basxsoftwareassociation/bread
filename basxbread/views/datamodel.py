21da8c879c5a785074e7af60753aaf2bb1e6bd56
Sam <sam@basx.dev>
2023-03-17 18:15:16 +0700

    add: more features for data model explorer

--- basxbread/views/datamodel.py
+++ basxbread/views/datamodel.py
@@ -2,27 +2,38 @@ import htmlgenerator as hg
 from django import forms
 from django.apps import apps
 from django.contrib.contenttypes.fields import GenericForeignKey
+from django.utils.translation import gettext_lazy as _
 
 from .. import layout, utils
 
 
 def fqn(model):
-    return model.__module__ + "." + model.__qualname__
+    result = model.__module__.split(".", 1)[0] + "." + model.__qualname__
+    if result.startswith("django."):
+        return model.__qualname__
+    return result
 
 
 def fieldentry(context):
     field = context["field"]
-    ret = hg.BaseElement(hg.SPAN(field.name, style="font-weight: bold"))
+    label = hg.DIV(
+        hg.SPAN(
+            " + ",
+            style="font-size: 1.2rem; cursor: pointer; user-select: none;",
+            onclick="infoblock = this.parentElement.nextElementSibling;"
+            "infoblock.style.display = infoblock.style.display == 'block' ? 'none' : 'block'",
+        ),
+        hg.SPAN(field.name, style="font-weight: bold"),
+    )
     if hasattr(field, "verbose_name"):
-        ret.append(" (" + field.verbose_name + ")")
-    ret.append(": ")
+        label.append(" (" + field.verbose_name + ")")
     typename = hg.BaseElement(fqn(type(field)))
     if field.is_relation:
         if isinstance(field, GenericForeignKey):
-            typename.append(f"through ({field.ct_field}, {field.fk_field})")
+            typename.append(f" through ({field.ct_field}, {field.fk_field})")
         else:
-            typename.append(" to ")
-            typename.append(
+            link = hg.SPAN(
+                " -> ",
                 hg.A(
                     fqn(field.target_field.model),
                     href=utils.reverse(
@@ -32,10 +43,14 @@ def fieldentry(context):
                             "highlight": field.target_field.model.__name__,
                         },
                     ),
-                )
+                ),
             )
-    ret.append(hg.CODE(typename))
-    return ret
+            label.append(link)
+    details = hg.DIV(
+        hg.CODE(typename),
+        style="display: none; padding-left: 1rem; margin-top: 0.25rem",
+    )
+    return hg.BaseElement(label, details)
 
 
 def is_same_app_relation(field, currentapp):
@@ -46,20 +61,21 @@ def is_same_app_relation(field, currentapp):
     return False
 
 
-def modelbox(model, highlighted):
-    def fieldlist(context):
-        result = []
-        for field in hg.resolve_lazy(model, context)._meta.get_fields():
-            if getattr(field, "parent_link", False) is False:
-                result.append(field)
-        return result
+def fieldlist(model):
+    result = []
+    for field in model._meta.get_fields():
+        if getattr(field, "parent_link", False) is False:
+            result.append(field)
+    return result
 
+
+def modelbox(model, highlighted):
     return hg.DIV(
         hg.DIV(
             hg.H5(
                 hg.format(
                     "{} {}",
-                    hg.F(lambda c: hg.resolve_lazy(model, c).__class__.__name__),
+                    model.__name__,
                     hg.SPAN(
                         hg.format("({})", model._meta.verbose_name),
                         style="font-weight: normal",
@@ -70,7 +86,7 @@ def modelbox(model, highlighted):
         ),
         hg.UL(
             hg.Iterator(
-                hg.F(fieldlist),
+                fieldlist(model),
                 "field",
                 hg.LI(
                     hg.F(fieldentry),
@@ -81,13 +97,10 @@ def modelbox(model, highlighted):
         ),
         style=hg.format(
             "border: solid {}; margin: 1rem;",
-            hg.If(
-                hg.F(lambda c: c["themodel"].__name__ == highlighted),
-                "orange 3px",
-                "black 1px",
-            ),
+            "orange 3px" if model.__name__ == highlighted else "black 1px",
         ),
-        id=hg.F(lambda c: c["themodel"].__name__),
+        id=model.__name__,
+        _class="modelbox",
     )
 
 
@@ -107,23 +120,60 @@ def datamodel(request):
     if form.is_valid():
         for model in apps.get_app_config(form.cleaned_data["app"]).get_models():
             models.append(model)
+    searchfunc = """
+for(model of $$(".modelbox h5")) {
+    let container = model.closest(".modelbox");
+    if(this.value == "") {
+        container.style.display = "initial";
+        model.style.backgroundColor = "initial";
+        for(field of $$("li", container)) {
+            field.style.backgroundColor = "initial";
+        }
+    } else {
+        if(model.innerText.toLowerCase().includes(this.value.toLowerCase())) {
+            container.style.display = "initial";
+            model.style.backgroundColor = "yellow";
+        } else {
+            container.style.display = "none";
+            model.style.backgroundColor = "initial";
+            for(field of $$("li", container)) {
+                field.style.backgroundColor = "initial";
+                if(field.innerText.toLowerCase().includes(this.value.toLowerCase())) {
+                    container.style.display = "initial";
+                    field.style.backgroundColor = "yellow";
+                }
+            }
+        }
+    }
+}
+"""
 
     return hg.BaseElement(
         hg.H1("Datamodel"),
         hg.DIV(
+            layout.forms.widgets.TextInput(
+                inputelement_attrs={
+                    "placeholder": _("Search..."),
+                    "onkeyup": searchfunc,
+                    "style": "box-shadow: 0px 5px 10px gray",
+                },
+                style="position: sticky; top: 4rem; margin: 0 auto; max-width: 20rem; height: 0;",
+            ),
             layout.forms.Form(
                 form,
-                layout.forms.FormField("app", onclick="this.closest('form').submit()"),
+                layout.forms.FormField(
+                    "app",
+                    onclick="this.closest('form').submit()",
+                    no_wrapper=True,
+                    style="display: inline-block",
+                ),
                 method="GET",
             ),
             hg.DIV(
-                hg.Iterator(
-                    models,
-                    "themodel",
-                    modelbox(hg.C("themodel"), request.GET.get("highlight")),
-                ),
-                style="display: flex; flex-wrap: wrap",
+                *[modelbox(m, request.GET.get("highlight")) for m in models],
+                style="display: flex; flex-wrap: wrap; align-items: flex-start; margin-top: 1rem; margin-left: -1rem; margin-right: -1rem",
             ),
+            style="margin-top: 2rem",
         ),
         hg.SCRIPT(
             hg.mark_safe(
